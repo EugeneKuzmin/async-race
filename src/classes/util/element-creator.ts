2569d8751f5ee082599e4e77accbe3ef315abb14
Gennarro@yandex.ru <Gennarro@yandex.ru>
2024-03-31 14:29:12 +0300

    feat:add main class to create elements

---
+++ src/classes/util/element-creator.ts
@@ -0,0 +1,53 @@
+
+type ElementParams = 
+{
+    tag: string,
+    classNames: string[],
+    textContent: string,
+    callback: () => void;
+}
+export default class ElementCreator {
+   
+   private element: HTMLElement | null;
+   constructor(params:ElementParams) {
+       this.element = null;
+       this.createElement(params);
+   }
+
+   getElement():HTMLElement  | null {
+       return this.element;
+   }
+
+ 
+   addInnerElement(element:HTMLElement | ElementCreator):void {
+       if (element instanceof ElementCreator) {
+           this.element?.append(element.getElement()!);
+       } else {
+           this.element?.append(element);
+       }
+   }
+
+    createElement(params:ElementParams):void {
+       this.element = document.createElement(params.tag);
+       this.setCssClasses(params.classNames);
+       this.setTextContent(params.textContent);
+       this.setCallback(params.callback);
+   }
+ 
+   setCssClasses(cssClasses:string[] = []) {
+       cssClasses.map((cssClass) => this.element?.classList.add(cssClass));
+   }
+
+   setTextContent(text:string = ''):void {
+    if (this.element) {
+       this.element.textContent = text;
+    }
+   }
+   
+   setCallback(callback: (event: Event) => void): void {
+    if (callback) {
+        this.element?.addEventListener('click', callback);
+    }
+
+    }
+}
